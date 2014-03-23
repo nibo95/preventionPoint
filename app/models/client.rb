24f3f7bd89693ce0f9c4d0bd839681b3986186d1
Nicholas Byron <nbyron@andrew.cmu.edu>
2014-03-23 16:44:23 -0400

    added timeliness gem and touched up validations accordingly

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,8 +4,8 @@ class Client < ActiveRecord::Base
 	has_many :exchanges, through: :visits
 
 	validates_presence_of :code
-	validates_presence_of :birth_date
-	validates_presence_of :register_date
+	validates_date :birth_date
+	validates_date :register_date
 	validates_presence_of :register_location_name
 	validates_presence_of :gender
 	validates_format_of :gender, with: /^[MFT]$/, message: "Please select valid gender option"
@@ -20,5 +20,5 @@ class Client < ActiveRecord::Base
 	validates_presence_of :hep_c_pos
 
 	scope :new, where('new = ?', true)
-	
+
 end
