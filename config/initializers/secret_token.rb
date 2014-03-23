24d8c1c8db2f90edcd47792683d1235387c4891c
Nicholas Byron <nbyron@andrew.cmu.edu>
2014-03-23 15:47:45 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PreventionPoint::Application.config.secret_key_base = '8743760e2b7861826fc0e0ef93c157357c0e3a2f7c5c2407ae2cbc81ef40fa276c1ec42e17ddb3f3ad7256f028cac3e0be1d37fe242943345be6bb21a0cb4f1b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PreventionPoint::Application.config.session_store :cookie_store, key: '_preventionPoint_session'
