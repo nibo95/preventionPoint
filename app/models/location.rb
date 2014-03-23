6f83a3de91ccb47dfa56ce6ba42ef448ac6909a0
Nicholas Byron <nbyron@andrew.cmu.edu>
2014-03-23 17:21:22 -0400

    added home view

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,6 @@
 class Location < ActiveRecord::Base
 	has_many :visits
 	has_many :exchanges, through: :visits
+
+	validates_presence_of :name
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 PreventionPoint::Application.routes.draw do
+
+  resources :homes
+
+  get 'home', to: 'home#index', as: :home
+  get 'home/about', to: 'home#about', as: :about
+  get 'home/contact', to: 'home#contact', as: :contact
+  get 'home/privacy', to: 'home#privacy', as: :privacy
+  get 'home/search', to: 'home#search', as: :search
+
   resources :users
-
   resources :exchanges
-
   resources :locations
-
   resources :visits
-
   resources :clients
+
+  root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
