1b14504a2fc94e5a3aef2169a192729c0287467c
Nicholas Byron <nbyron@andrew.cmu.edu>
2014-03-23 16:59:39 -0400

    added validations and some scopes to visit model

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,5 +1,13 @@
 class Visit < ActiveRecord::Base
+	
 	belongs_to :client
 	has_one :exchange
 	belongs_to :location
+	
+	validates :exchange_id, presence: true, numericality: { greater_than: 0, only_integer: true }
+	validates :location_id, presence: true, numericality: { greater_than: 0, only_integer: true }
+	validates :client_id, presence: true, numericality: { greater_than: 0, only_integer: true }
+	validates_date :visit_time, :on_or_before => Date.today
+
+	scope :chronological, order('visit_time')
 end
